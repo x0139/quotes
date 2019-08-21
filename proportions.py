18d6ac7b04e31a652f425cec2c8ca003a9db8765
x0139 <oleg_a@outlook.com>
2019-08-21 12:09:42 +0200

    changes in proportions, float to decimal

--- proportions.py
+++ proportions.py
@@ -1,9 +1,7 @@
-def calculate_rates(rate: float, total: float, precision: int = 3):
-    return round(rate / total, precision)
-
+from decimal import *
 
 if __name__ == '__main__':
-    rates = [float(input()) for _ in range(int(input()))]
+    rates = [Decimal(input()) for _ in range(int(input()))]
     total = sum(rates)
     for rate in rates:
-        print(calculate_rates(rate, total))
+        print(round(rate / total, 3))
