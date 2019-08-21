f2ef1e0aeb4924f0a68382a4dea580e547e5a561
x0139 <oleg_a@outlook.com>
2019-08-21 12:46:09 +0200

    add getcontext for decimal precision

--- proportions.py
+++ proportions.py
@@ -1,7 +1,9 @@
 from decimal import *
 
 if __name__ == '__main__':
+    getcontext().prec = 3
     rates = [Decimal(input()) for _ in range(int(input()))]
     total = sum(rates)
     for rate in rates:
-        print(round(rate / total, 3))
+        print(round(rate / total, 3)
+
