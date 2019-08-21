bef6271f5f31b5f941678d82dae6ff646143f1b2
x0139 <oleg_a@outlook.com>
2019-08-21 12:46:32 +0200

    make const for precision

--- proportions.py
+++ proportions.py
@@ -1,9 +1,11 @@
 from decimal import *
 
+PRECISION = 3
+
 if __name__ == '__main__':
-    getcontext().prec = 3
+    getcontext().prec = PRECISION
     rates = [Decimal(input()) for _ in range(int(input()))]
     total = sum(rates)
     for rate in rates:
-        print(round(rate / total, 3)
+        print(round(rate / total, PRECISION)
 
