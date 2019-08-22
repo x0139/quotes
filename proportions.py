fb37ce37d6759c60cf171ee6ed56046152834568
x0139 <oleg_a@outlook.com>
2019-08-22 15:34:46 +0200

    Decimal to float()

--- proportions.py
+++ proportions.py
@@ -1,7 +1,4 @@
 import operator
-from decimal import *
-
-PRECISION = 3
 
 
 def round_down(x: float) -> int:
@@ -17,8 +14,7 @@ def round_percentage(props: list):
 
 
 if __name__ == '__main__':
-    getcontext().prec = PRECISION
-    rates = [Decimal(input()) for _ in range(int(input()))]
+    rates = [float(input()) for _ in range(int(input()))]
     total = sum(rates)
     proportions = [round_down(rate / total) for rate in rates]
     if sum(proportions) != 100:
